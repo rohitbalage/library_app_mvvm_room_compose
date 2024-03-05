b62ea0bc35270d45711eaed285087bf50f8efa38
Rohit Balage <thatcomputerboy26@gmail.com>
2024-03-04 19:21:08 -0500

    setup room dependency and project

--- app/src/main/java/com/rrbofficial/libraryapp/MainActivity.kt
+++ app/src/main/java/com/rrbofficial/libraryapp/MainActivity.kt
@@ -3,8 +3,12 @@ package com.rrbofficial.libraryapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,25 +26,28 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                  MainScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingPreview() {
-    LibraryAppTheme {
-        Greeting("Android")
+fun MainScreen(){
+    Column {
+        OutlinedTextField(value = "",
+            onValueChange ={},
+            label = { Text(text = "Enter the Book Name")},
+            placeholder = { Text(text = " Enter Your Book Name")}
+        )
+        Button(onClick = { }) {
+
+            Text(text = "Insert Book into DB")
+
+        }
     }
+
 }
